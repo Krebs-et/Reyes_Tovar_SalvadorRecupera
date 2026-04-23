eb158021af49dea152151df782d409f2fcd9a1cc
SanDaniel777 <20243ds156@utez.edu.mx>
2026-04-23 07:21:48 -0600

    Initial commit

---
+++ src/main/java/com/ingenium/reyes_tovar_salvador5c/ReyesTovarSalvador5CApplication.java
@@ -0,0 +1,13 @@
+package com.ingenium.reyes_tovar_salvador5c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReyesTovarSalvador5CApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReyesTovarSalvador5CApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ingenium/reyes_tovar_salvador5c/ReyesTovarSalvador5CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenium.reyes_tovar_salvador5c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReyesTovarSalvador5CApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
